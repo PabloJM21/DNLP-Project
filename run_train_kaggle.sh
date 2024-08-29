3e74608d406219bbfe03f9ad9cfdc76e13379f83
PabloJM21 <pablo.jahnen@stud.uni-goettingen.de>
2024-08-29 23:34:39 +0200

    Create run_train_kaggle.sh

---
+++ run_train_kaggle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Printing out some environment info (for debugging)
+echo "Running script in Kaggle environment"
+echo "Working directory: $PWD"
+echo "Current node: $(hostname)"
+
+# For debugging purposes.
+python --version
+python -m torch.utils.collect_env 2> /dev/null
+
+# Run the modified script
+python -u /kaggle/input/dlfnlp/dlfnlp-sose2024/multitask_classifier_kaggle.py --use_gpu --option finetune --task sts --hidden_dropout_prob 0.1
